4ba6fd97f7de6dd599105f7c8d6ca59894aca63f
farbod <farbod.ps@gmail.com>
2020-06-18 15:40:09 +0430

    add ui dagger module

---
+++ Snapp/app/src/main/java/com/snapp/interview/injection/module/UiModule.kt
@@ -0,0 +1,21 @@
+package com.snapp.interview.injection.module
+
+import com.snapp.domain.executor.PostExecutionThread
+import com.snapp.interview.executor.UiThread
+import com.snapp.interview.ui.MainActivity
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+/**
+ * Module that provides all dependencies from the mobile-ui package/layer and injects all activities.
+ */
+@Module
+abstract class UiModule {
+
+    @Binds
+    abstract fun bindPostExecutionThread(uiThread: UiThread): PostExecutionThread
+
+    @ContributesAndroidInjector
+    abstract fun provideMainActivity(): MainActivity
+}
