bf34cb8052b1085d54a4cb2b2bbc371c20ecf884
farbod <farbod.ps@gmail.com>
2020-06-18 15:48:59 +0430

    inject main activity

--- Snapp/app/src/main/java/com/snapp/interview/ui/MainActivity.kt
+++ Snapp/app/src/main/java/com/snapp/interview/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.snapp.interview.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.snapp.interview.R
+import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
