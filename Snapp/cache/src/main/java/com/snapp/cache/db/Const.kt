7411bc85c365e66cdd392bacd6e9f475e8b7b845
farbod <farbod.ps@gmail.com>
2020-06-18 14:51:56 +0430

    create room database

--- Snapp/cache/src/main/java/com/snapp/cache/db/Const.kt
+++ Snapp/cache/src/main/java/com/snapp/cache/db/Const.kt
@@ -5,6 +5,8 @@ package com.snapp.cache.db
  */
 object Const {
 
+    const val DATABASE_NAME = "snapp.db"
+
     const val TABLE_NAME = "vehicles"
 
     const val QUERY_ALL_VEHICLES = "SELECT * FROM" + " " + TABLE_NAME
