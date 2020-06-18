5eb94bdf3b45823ae4a214e7c780738b9508c818
farbod <farbod.ps@gmail.com>
2020-06-18 16:17:50 +0430

    check internet connection to show appropriate fragment

--- Snapp/app/src/main/java/com/snapp/interview/ui/MainActivity.kt
+++ Snapp/app/src/main/java/com/snapp/interview/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.snapp.interview.ui
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.snapp.interview.R
+import com.snapp.interview.ui.browse.BrowseFragment
+import com.snapp.interview.ui.map.MapFragment
 import dagger.android.AndroidInjection
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +16,24 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            when {
+                isConnectedToInternet() -> replaceFragment(MapFragment.newInstance())
+                else -> replaceFragment(BrowseFragment.newInstance())
+            }
+        }
+    }
+
+    private fun isConnectedToInternet(): Boolean {
+        val connectivity =
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        return connectivity?.activeNetworkInfo?.isConnectedOrConnecting ?: false
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
     }
 }
