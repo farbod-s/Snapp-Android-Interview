4715472d279916d2ae53b26d59dd855ae566592a
farbod <farbod.ps@gmail.com>
2020-06-18 14:52:55 +0430

    add preferences helper

---
+++ Snapp/cache/src/main/java/com/snapp/cache/PreferencesHelper.kt
@@ -0,0 +1,33 @@
+package com.snapp.cache
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * General Preferences Helper class, used for storing preference values using the Preference API
+ */
+@Singleton
+open class PreferencesHelper @Inject constructor(context: Context) {
+
+    companion object {
+        private const val PREF_PACKAGE_NAME = "com.snapp.interview.preferences"
+
+        private const val PREF_KEY_LAST_CACHE = "last_cache"
+    }
+
+    private val sharedPreferences: SharedPreferences
+
+    init {
+        sharedPreferences =
+            context.getSharedPreferences(PREF_PACKAGE_NAME, Context.MODE_PRIVATE)
+    }
+
+    /**
+     * Store and retrieve the last time data was cached
+     */
+    var lastCacheTime: Long
+        get() = sharedPreferences.getLong(PREF_KEY_LAST_CACHE, 0)
+        set(lastCache) = sharedPreferences.edit().putLong(PREF_KEY_LAST_CACHE, lastCache).apply()
+}
